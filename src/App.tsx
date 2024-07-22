c12d10c1bc593a4a4b02a478b5b8de892ec3fa66
Charlie Payne <charliepayne1101@gmai.com>
2024-07-22 20:24:07 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='bg-black flex-container'>
+        <div className='text-white content'>
+          <h1>Collagen Lab</h1>
+          <h2>COMING SOON</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
