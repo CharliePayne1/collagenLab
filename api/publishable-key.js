d626273880d606fe0429fca6fe48666110ee8938
Charlie Payne <charliepayne1101@gmai.com>
2024-08-05 22:18:56 +0100

    add routes

--- api/publishable-key.js
+++ api/publishable-key.js
@@ -1,5 +1,4 @@
 export default function handler(_, res) {
-  console.log("Stripe Publishable Key:", process.env.STRIPE_PUBLISHABLE_KEY);
   res.send({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY });
 }
   
